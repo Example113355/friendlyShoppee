1b096d3edc7b95df9814d3d16db8b5ccf3d20825
machtrangcuc <161485269+machtrangcuc@users.noreply.github.com>
2024-05-28 23:33:13 +0700

    Update item.jsx

--- frontend/src/components/item.jsx
+++ frontend/src/components/item.jsx
@@ -5,7 +5,7 @@ const Item = ( {img, name, price, id} ) => {
     const addToCart = async () => {
         try {
             const userId = localStorage.getItem('userId');
-            await axios.post('https://friendly-shoppee-7u3u.vercel.app/api/cart/add', JSON.stringify({ 
+            await axios.post('https://friendly-shoppee-4v8j.vercel.app/api/cart/add', JSON.stringify({ 
                 "userId": userId, 
                 "itemId": id,
                 "quantity": 1,
